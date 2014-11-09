133c2c6ac0b0083d5c1bb7810e2fee5655bea000
tykayn-ubuntu <tykayn@gmail.com>
2014-11-09 16:55:13 +0100

    début du jeu

---
+++ deckOfCards.js
@@ -0,0 +1,23 @@
+/**
+ * deck of cards
+ * @returns {undefined}
+ */
+function Deck(){
+    
+}
+
+/**
+ * card player
+ * @returns {undefined}
+ */
+function Player(){
+    
+}
+
+/**
+ * any card
+ * @returns {undefined}
+ */
+function Card(){
+    
+}
